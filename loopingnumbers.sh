95f767228ab5ed88028bad18b4815be98e7a6c35
Maria Dekova <msDekova@Marias-MacBook-Air.local>
2020-06-12 23:54:37 +0300

    Add solution

---
+++ loopingnumbers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Use for loops to display the natural numbers from 1 to 50.
+
+for number in {1..50}
+ do 
+    echo $number
+done
+
---
+++ numbers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Given two integers, X and Y, find their sum, difference, product, and quotient.
+
+read X
+read Y
+
+echo $((X+Y))
+echo $((X-Y))
+echo $((X*Y))
+echo $((X/Y))
+
