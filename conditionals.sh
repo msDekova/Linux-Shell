41c0eb37e35861f0936108da09d2e0de6dfe75d7
Maria Dekova <msDekova@Marias-MacBook-Air.local>
2020-06-13 00:20:31 +0300

    Add solution

--- conditionals.sh
+++ conditionals.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 #Read in one character from the user (this may be 'Y', 'y', 'N', 'n'). If the character is 'Y' or 'y' display "YES". If the character is 'N' or 'n' display "NO". No other character will be provided as input.
 
-read X
-read Y
-read Z
-
-if [ $X -ne $Y ] && [ $X -ne $Z ] && [ $Y -ne $Z ]
-then
-    echo SCALENE;
-elif [ $X -eq $Y ] && [ $X -eq $Z ] && [ $Y -eq $Z ]
-then 
-    echo EQUILATERAL;
-else
-    echo ISOSCELES;
-fi
+read character
+case $character in
+    [Yy]) echo "YES";;
+    [Nn]) echo "NO";;
+esac
 
 
