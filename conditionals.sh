88e1c941a758e1bf66f55f116ed2bdf863cd9290
Maria Dekova <msDekova@Marias-MacBook-Air.local>
2020-06-13 00:00:11 +0300

    Add solution

---
+++ conditionals.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Read in one character from the user (this may be 'Y', 'y', 'N', 'n'). If the character is 'Y' or 'y' display "YES". If the character is 'N' or 'n' display "NO". No other character will be provided as input.
+
+read character
+case $character in
+    [Yy]) echo "YES";;
+    [Nn]) echo "NO";;
+esac
+
