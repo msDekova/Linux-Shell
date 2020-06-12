5e3a3c6577e748cf1279621b9f3af0adb5266553
Maria Dekova <msDekova@Marias-MacBook-Air.local>
2020-06-12 21:25:16 +0300

    Add solution

---
+++ loopingskipping.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Your task is to use for loops to display only odd natural numbers from 1 to 99.
+
+for number in {1..99..2}
+do 
+   echo $number
+done
+
---
+++ personalizedecho.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Write a Bash script which accepts NAME as input and displays a greeting: "Welcome (name)"
+
+read NAME
+echo "Welcome" $NAME
