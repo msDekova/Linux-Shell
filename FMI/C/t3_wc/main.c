690ccd4e5f0fe5c71b66d6d7af0f784e50a57dda
Maria Dekova <msDekova@Marias-MacBook-Air.local>
2020-08-03 16:24:44 +0300

    wc command

---
+++ FMI/C/t3_wc/main.c
@@ -0,0 +1,48 @@
+/*
+Реализирайте команда wc, с един аргумент подаден като входен параметър
+*/
+
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <err.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	
+	int fd;
+	char c;
+	int lines = 0;
+	int words = 0;
+	int chars = 0;
+	const char* filename = argv[1];
+	int rd;
+
+	if( argc != 2 ) {
+		err(1,"Invalid number of arguments\n");
+	}
+
+	if ( (fd = open(filename, O_RDONLY) ) == -1 ) {
+		err(2, "File failed to open in read mode\n");
+	}
+
+	while ( ( rd = read(fd, &c, 1) > 0 ) ) {
+		if ( c == '\n' ) {
+			lines++;
+			words++;
+		}
+
+		if ( c == ' ' ) {
+			words++;
+		}
+
+		chars++;
+	}
+
+	close(fd);
+	printf("%d   %d   %d  %s\n", lines, words, chars, filename);
+	exit(0);
+}
