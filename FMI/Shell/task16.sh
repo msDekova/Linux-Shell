5de8a36677148024203efab48c3a7b9083c868f4
Maria Dekova <msdekova1998@gmail.com>
2020-08-26 23:03:06 +0300

    Add solution

---
+++ FMI/Shell/task16.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]
+then
+	echo "Invalid number of arguments"
+	exit 1
+fi
+
+if [ ! -f ${1} ]
+then
+	echo "First argument must be regular file"
+	exit 2
+fi
+
+
+FILE=$1
+STRING1=$2
+STRING2=$3
+
+TERM1=$(cat ${FILE} | grep "${STRING1}=" | cut -d '=' -f2 | tr ' ' '\n' | sort | uniq | sed 1d )
+TERM2=$(cat ${FILE} | grep "${STRING2}=" | cut -d '=' -f2 | tr ' ' '\n' | sort | uniq )
+echo "${TERM1}"
+echo "${TERM2}"
+
+RES=$(echo "${TERM2}" | grep -v "${TERM1}" | tr '\n' ' ')
+echo "${RES}"
+_TERM1=$(echo "${TERM1}" | tr '\n' ' ' )
+_TERM2=$(echo "${TERM2}" | tr '\n' ' ' )
+
+#echo "${RES}"
+sed -i -e "s/^${STRING2}=.*/${STRING2}=${RES}/" "${FILE}"
